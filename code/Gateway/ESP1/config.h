5f588f3f44fb15f1c7b8feda35983b432459d45e
PricelessToolkit <pricelesstoolkit@gmail.com>
2024-02-13 22:02:53 +0100

    Update config.h

--- code/Gateway/ESP1/config.h
+++ code/Gateway/ESP1/config.h
@@ -1,6 +1,6 @@
 /////////////////////////// LoRa/ESPNow Gateway Key ///////////////////////////
 
-#define GATEWAY_KEY "abc" // Keep it small
+#define GATEWAY_KEY "ab" // Keep it small
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -30,10 +30,10 @@
 // due to the decreased data rate, which is a trade-off for increased communication range and signal robustness.
 
 #define SIGNAL_BANDWITH 125E3  // signal bandwidth in Hz, defaults to 125E3
-#define SPREADING_FACTOR 12    // ranges from 6-12,default 7 see API docs
+#define SPREADING_FACTOR 8    // ranges from 6-12,default 7 see API docs
 #define CODING_RATE 5          // Supported values are between 5 and 8, these correspond to coding rates of 4/5 and 4/8. The coding rate numerator is fixed at 4.
 #define SYNC_WORD 0xF3         // byte value to use as the sync word, defaults to 0x12
-#define PREAMBLE_LENGTH 8      // Supported values are between 6 and 65535.
+#define PREAMBLE_LENGTH 6      // Supported values are between 6 and 65535.
 #define TX_POWER 20            // TX power in dB, defaults to 17, Supported values are 2 to 20
 #define BAND 433E6             // 433E6 / 868E6 / 915E6
 
